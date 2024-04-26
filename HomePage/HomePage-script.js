47cb00f3df6e8cae8c832751acfaf78f8a27411b
miacheng2 <miacheng23@gmail.com>
2024-04-26 22:27:03 +0800

    updated the login page by adding a background image, updated the login and logout api calls to ensure the local stroages are correctly configured by adding JWT token, user name and user id for later authentication. The index.html is created for the home page and it display all posts and related replys accordingly, the post and reply only displays while the user is logged in. Addtionally user can reply each of the listed posts by simply add comments to the input box below. The backend has been updated with login, signup, logout, get post, post post, and post reply apis with necessary method.

--- HomePage/HomePage-script.js
+++
@@ -1,9 +0,0 @@
-function changeDropdownText(text) {
-  var dropdownBtn = document.querySelector(".dropbtn");
-  dropdownBtn.textContent = text;
-}
-
-function changeDropdownTextForRange(text) {
-  var dropdownBtn = document.querySelector(".dropbtnForRange");
-  dropdownBtn.textContent = text;
-}
--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # CITS5505-Project2
 This is the readme file
 
-in order to run the project, run "pip install requirement.txt"
+in order to run the project, run "pip install -r requirement.txt"
 for new installed packages, run "pip freeze > requirement.txt"
