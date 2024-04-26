6230abc3b9758046399aac0928b80c54b10592cd
morepresent <164336733+morepresent@users.noreply.github.com>
2024-04-26 15:17:51 +0800

    I have created a basic homepage and will continue to refine it in the future.

---
+++ HomePage/script.js
@@ -0,0 +1,9 @@
+function changeDropdownText(text) {
+  var dropdownBtn = document.querySelector(".dropbtn");
+  dropdownBtn.textContent = text;
+}
+
+function changeDropdownTextForRange(text) {
+  var dropdownBtn = document.querySelector(".dropbtnForRange");
+  dropdownBtn.textContent = text;
+}
