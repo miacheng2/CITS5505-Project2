94419cecf995aa464324d79b19f575b75c77e79f
Zac <haozhil97@gmail.com>
2024-04-28 20:49:32 +0800

    add js and more css styles to version1. so this is version 2

---
+++ profile/profile_js.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    $(".edit_button").click(function () {
+        $(".pro_edit").fadeIn();
+    });
+    $(".close_button").click(function () {
+        $(".pro_edit").fadeOut();
+    })
+
+})
