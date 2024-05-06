7e7e6516785742d27d18b7561358f4d684b45364
whatthehiao <125631364+whatthehiao@users.noreply.github.com>
2024-05-06 18:50:56 +0800

    Adapt functions in welcome page

--- models.py
+++ models.py
@@ -1,6 +1,7 @@
 # models.py
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin
+from datetime import datetime
 
 
 db = SQLAlchemy()
@@ -33,6 +34,7 @@ class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     userName = db.Column(db.String(80), nullable=False)
     passWord = db.Column(db.String(80), nullable=False)
+    registerDate = db.Column(db.DateTime, nullable=False, default=datetime.now)
     def __repr__(self):
         return f'<User {self.userName}>'
     
